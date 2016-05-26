c93d3e6e5d69e43eb2f43c7e1a965dc76426793a
Toshiki Tsuboi <t.tsubo2000@gmail.com>
2016-05-26 16:21:44 +0900

    option:"-u",remote_host

--- add_neighbor.py
+++ add_neighbor.py
@@ -3,23 +3,24 @@ import sys
 from fabric.api import local
 
 
-def add_neighbor(neighbor_address, peer_as):
-    cmd = 'gobgp -j neighbor add {0} as {1}'.format(neighbor_address, peer_as)
+def add_neighbor(neighbor_address, peer_as, mgmt_addr):
+    cmd = 'gobgp -j neighbor add {0} as {1} -u {2}'.format(neighbor_address, peer_as, mgmt_addr)
     ret = local(cmd, capture=True)
     return ret
 
 def main(argv):
     neighbor_address = argv[1]
     peer_as = argv[2]
-    ret = add_neighbor(neighbor_address, peer_as)
+    mgmt_addr = argv[3]
+    ret = add_neighbor(neighbor_address, peer_as, mgmt_addr)
     if ret == "":
         print "Success!"
     else:
         print "Error!"
 
 if __name__ == '__main__':
-    if (len(sys.argv) != 3):
-        print "Usage: python add_neighbor.py [neighbor_address] [peer_as]"
+    if (len(sys.argv) != 4):
+        print "Usage: python add_neighbor.py [neighbor_address] [peer_as] [mgmt_addr]"
         sys.exit()
     else:
         main(sys.argv)
--- global_config.py
+++ global_config.py
@@ -3,23 +3,24 @@ import sys
 from fabric.api import local
 
 
-def global_config(local_as, router_id):
-    cmd = 'gobgp -j global as {0} router-id {1}'.format(local_as, router_id)
+def global_config(local_as, router_id, mgmt_addr):
+    cmd = 'gobgp -j global as {0} router-id {1} -u {2}'.format(local_as, router_id, mgmt_addr)
     ret = local(cmd, capture=True)
     return ret
 
 def main(argv):
     local_as = argv[1]
     router_id = argv[2]
-    ret = global_config(local_as, router_id)
+    mgmt_addr = argv[3]
+    ret = global_config(local_as, router_id, mgmt_addr)
     if ret == "":
         print "Success!"
     else:
         print "Error!"
 
 if __name__ == '__main__':
-    if (len(sys.argv) != 3):
-        print "Usage: python global_config.py [local_as] [router_id]"
+    if (len(sys.argv) != 4):
+        print "Usage: python global_config.py [local_as] [router_id], [mgmt_addr]"
         sys.exit()
     else:
         main(sys.argv)
